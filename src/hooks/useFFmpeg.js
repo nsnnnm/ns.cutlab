cf68a3d23c4ff0bb14a8e6f9c4a1b9d28b88a860
nsnnnm <nekosuke.1012@proton.me>
2026-03-20 12:44:09 +0900

    fix: use jsdelivr for ffmpeg core

--- src/hooks/useFFmpeg.js
+++ src/hooks/useFFmpeg.js
@@ -23,7 +23,7 @@ export function useFFmpeg() {
         setLog(message)
       })
 
-      const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
+      const baseURL = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd'
       await ffmpeg.load({
         coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
         wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
